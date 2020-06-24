ef6813baa4b8c075c2d74392c42032401db31358
Anton Ihonen <anton.ihonen@tuni.fi>
2020-06-24 15:46:56 +0300

    Add colors to log messages for clarity

--- tester/core/log.py
+++ tester/core/log.py
@@ -8,13 +8,43 @@ This module defines functionality related to logging.
 import logging
 import sys
 
+import colorama
+colorama.init()
+
+# Custom formatter
+class ColoredFormatter(logging.Formatter):
+    """A formatter that adds color to log messages based on the logging level.
+    Meant to be used with the console logger so the user can more easily
+    filter information based on the color."""
+    def __init__(self, format):
+        super().__init__(fmt=format, datefmt=None, style='%')
+        self.debug_format = f"{colorama.Fore.LIGHTBLACK_EX}{format}{colorama.Fore.RESET}"
+        self.info_format = f"{colorama.Fore.GREEN}{format}{colorama.Fore.RESET}"
+        self.warning_format = f"{colorama.Fore.LIGHTYELLOW_EX}{format}{colorama.Fore.RESET}"
+        self.error_format = f"{colorama.Fore.RED}{format}{colorama.Fore.RESET}"
+
+    def format(self, record):
+        original_format = self._style._fmt
+        if record.levelno == logging.DEBUG:
+            self._style._fmt = self.debug_format
+        elif record.levelno == logging.INFO:
+            self._style._fmt = self.info_format
+        elif record.levelno == logging.WARNING:
+            self._style._fmt = self.warning_format
+        elif record.levelno == logging.ERROR:
+            self._style._fmt = self.error_format
+        result = logging.Formatter.format(self, record)
+        self._style._fmt = original_format
+        return result
+
 # Set up global console logger.
-formatter = logging.Formatter("--%(levelname)s: %(message)s")
+formatter = ColoredFormatter("--%(levelname)s: %(message)s")
 handler = logging.StreamHandler(sys.stdout)
-console_logger = logging.getLogger("console")
 handler.setFormatter(formatter)
+console_logger = logging.getLogger("console")
 console_logger.addHandler(handler)
 console_logger.setLevel(logging.DEBUG)
+
 
 def setup_build_logger(log_filename: str) -> logging.Logger:
     """Initializes and returns a Logger object with the given filename.
