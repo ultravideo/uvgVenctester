ultravideo/uvgVenctester

commit 773da45c2ecb21395878d8b33909f3d80ca4d55c

Simplify the interface for ffmpeg metric calculation
Can be probably still simplified but anyways makes adding additional metrics
 easier

tester/core/ffmpeg.py
@@ -7,6 +7,7 @@ import re
 import shutil
 import subprocess
 from pathlib import Path
+from typing import Dict
 
 import tester.core.csv as csv
 import tester.core.test as test
@@ -17,6 +18,12 @@ from tester.core.log import console_log
 _PSNR_PATTERN: re.Pattern = re.compile(r".*psnr_avg:([0-9]+.[0-9]+).*", re.DOTALL)
 _SSIM_PATTERN: re.Pattern = re.compile(r".*All:([0-9]+.[0-9]+).*", re.DOTALL)
 _VMAF_PATTERN: re.Pattern = re.compile(r".*\"VMAF score\":([0-9]+.[0-9]+).*", re.DOTALL)
+
+_PATTERNS = {
+    "psnr":_PSNR_PATTERN,
+    "ssim": _SSIM_PATTERN,
+    "vmaf": _VMAF_PATTERN
+}
 
 
 def ffmpeg_validate_config():
@@ -35,22 +42,17 @@ def ffmpeg_validate_config():
         raise RuntimeError
 
 
-def compute_metrics(encoding_run: test.EncodingRun,
-                    psnr: bool,
-                    ssim: bool,
-                    vmaf: bool) -> (float, float, float):
+def compute_metrics(encoding_run: test.EncodingRun, metrics: list) -> Dict[str: float]:
 
-    if not psnr and not ssim and not vmaf:
+    if not metrics:
         return None, None, None
 
     assert encoding_run.output_file.get_filepath().exists()
     # Absolute paths were causing trouble, so use relative paths.
-    psnr_log_name = encoding_run.psnr_log_path.name
-    ssim_log_name = encoding_run.ssim_log_path.name
-    vmaf_log_name = encoding_run.vmaf_log_path.name
+    logs = {x: encoding_run.get_log_path(x) for x in metrics}
 
     # Copy the VMAF model into the working directory to enable using a relative path.
-    if vmaf:
+    if "vmaf" in metrics:
         vmaf_model_src_path1 = cfg.Cfg().vmaf_repo_path / "model" / "vmaf_v0.6.1.pkl"
         vmaf_model_src_path2 = cfg.Cfg().vmaf_repo_path / "model" / "vmaf_v0.6.1.pkl.model"
         vmaf_model_dest_path1 = encoding_run.output_file.get_filepath().parent / "vmaf_v0.6.1.pkl"
@@ -60,7 +62,7 @@ def compute_metrics(encoding_run: test.EncodingRun,
 
     # Build the filter based on which metrics are to be computed:
 
-    no_of_metrics = sum(int(boolean) for boolean in (psnr, ssim, vmaf))
+    no_of_metrics = len(metrics)
 
     # Adjust for frame step (it could be that only every <step>th frame of the input sequence was encoded).
     split1 = f"[0:v]select=not(mod(n\\,{cfg.Cfg().frame_step_size}))[select1_out]; " \
@@ -68,22 +70,22 @@ def compute_metrics(encoding_run: test.EncodingRun,
     split2 = f"[1:v]split={no_of_metrics}"
     filters = []
 
-    if psnr:
+    if "psnr" in metrics:
         split1 += "[yuv_psnr]"
         split2 += "[hevc_psnr]"
         filters.append(f"[hevc_psnr][yuv_psnr]"
-                       f"psnr=stats_file={psnr_log_name}")
-    if ssim:
+                       f"psnr=stats_file={logs['psnr'].name}")
+    if "ssim" in metrics:
         split1 += "[yuv_ssim]"
         split2 += "[hevc_ssim]"
         filters.append(f"[hevc_ssim][yuv_ssim]"
-                       f"ssim=stats_file={ssim_log_name}")
-    if vmaf:
+                       f"ssim=stats_file={logs['ssim'].name}")
+    if "vmaf" in metrics:
         split1 += "[yuv_vmaf]"
         split2 += "[hevc_vmaf]"
         filters.append(f"[hevc_vmaf][yuv_vmaf]"
                        f"libvmaf=model_path={vmaf_model_dest_path1.name}:"
-                       f"log_path={vmaf_log_name}:"
+                       f"log_path={logs['vmaf'].name}:"
                        f"log_fmt=json")
 
     ffmpeg_filter = f"{split1}; "\
@@ -149,12 +151,8 @@ def compute_metrics(encoding_run: test.EncodingRun,
         console_log.debug(f"ffmpeg: Computing metrics")
         console_log.debug(f"ffmpeg: Input 1: '{encoding_run.input_sequence.get_filepath().name}'")
         console_log.debug(f"ffmpeg: Input 2: '{encoding_run.output_file.get_filepath().name}'")
-        if psnr:
-            console_log.debug(f"ffmpeg: PSNR log: '{psnr_log_name}'")
-        if ssim:
-            console_log.debug(f"ffmpeg: SSIM log: '{ssim_log_name}'")
-        if vmaf:
-            console_log.debug(f"ffmpeg: VMAF log: '{vmaf_log_name}'")
+        for m in metrics:
+            console_log.debug(f"ffmpeg: {m.upper()} log: '{m}'")
 
         subprocess.check_output(
             subprocess.list2cmdline(ffmpeg_command),
@@ -162,7 +160,7 @@ def compute_metrics(encoding_run: test.EncodingRun,
             shell=True
         )
 
-        if vmaf:
+        if "vmaf" in metrics:
             # Remove the temporary VMAF model file.
             os.remove(vmaf_model_dest_path1)
             os.remove(vmaf_model_dest_path2)
@@ -170,38 +168,17 @@ def compute_metrics(encoding_run: test.EncodingRun,
         if encoding_run.decoded_output_file_path:
             os.remove(encoding_run.decoded_output_file_path)
 
-        # Ugly but simple:
-
-        psnr_avg = None
-        if psnr:
-            frame_psnrs = []
-            with encoding_run.psnr_log_path.open("r") as psnr_log:
-                lines = psnr_log.readlines()
+        results = {}
+        for metric in metrics:
+            frame_results = []
+            with logs[metric].open("r") as log:
+                lines = log.readlines()
                 for line in lines:
-                    for item in _PSNR_PATTERN.fullmatch(line).groups():
-                        frame_psnrs.append(float(item))
-                psnr_avg = sum(frame_psnrs) / len(lines)
+                    for item in _PATTERNS[metric].fullmatch(line).groups():
+                        frame_results.append(float(item))
+                results[metric] = sum(frame_results) / len(frame_results)
 
-        ssim_avg = None
-        if ssim:
-            frame_ssims = []
-            with encoding_run.ssim_log_path.open("r") as ssim_log:
-                lines = ssim_log.readlines()
-                for line in lines:
-                    for item in _SSIM_PATTERN.fullmatch(line).groups():
-                        frame_ssims.append(float(item))
-                ssim_avg = sum(frame_ssims) / len(lines)
-
-        vmaf_avg = None
-        if vmaf:
-            with encoding_run.vmaf_log_path.open("r") as vmaf_log:
-                for line in vmaf_log.readlines():
-                    match = _VMAF_PATTERN.fullmatch(line)
-                    if match:
-                        vmaf_avg = float(match.groups()[0])
-                        break
-
-        return psnr_avg, ssim_avg, vmaf_avg
+        return results
 
     except Exception as exception:
         console_log.error(f"ffmpeg: Failed to compute metrics")

tester/core/conformance.py
@@ -24,7 +24,7 @@ def check_hevc_conformance(encoding_run: test.EncodingRun):
         "-b", encoding_run.output_file.get_filepath(),
         "-o", os.devnull
     )
-    with open(encoding_run.conformance_log_path, "w") as log:
+    with open(encoding_run.get_log_path("conformance"), "w") as log:
         try:
             handle = subprocess.Popen(
                 cmd,

tester/core/test.py
@@ -43,18 +43,13 @@ class EncodingRun:
 
         qp_name = self.qp_name.short_name
 
-        base_filename = f"{input_sequence.get_filepath().with_suffix('').name}_" \
+        self.base_filename = f"{input_sequence.get_filepath().with_suffix('').name}_" \
                         f"{qp_name}{self.param_set.get_quality_param_value()}_{round_number}"
-        output_dir_path = encoder.get_output_dir(param_set)
+        self.output_dir_path = encoder.get_output_dir(param_set)
 
-        self.encoding_log_path: Path = output_dir_path / f"{base_filename}_encoding_log.txt"
-        self.metrics_path: Path = output_dir_path / f"{base_filename}_metrics.json"
-        self.psnr_log_path: Path = output_dir_path / f"{base_filename}_psnr_log.txt"
-        self.ssim_log_path: Path = output_dir_path / f"{base_filename}_ssim_log.txt"
-        self.vmaf_log_path: Path = output_dir_path / f"{base_filename}_vmaf_log.txt"
-        self.conformance_log_path: Path = output_dir_path / f"{base_filename}_conformance_log.txt"
+        self.metrics_path: Path = self.output_dir_path / f"{self.base_filename}_metrics.json"
 
-        output_file_path: Path = output_dir_path / f"{base_filename}.{encoder.file_suffix}"
+        output_file_path: Path = self.output_dir_path / f"{self.base_filename}.{encoder.file_suffix}"
         self.output_file = EncodedVideoFile(
             filepath=output_file_path,
             width=input_sequence.get_width(),
@@ -68,7 +63,7 @@ class EncodingRun:
 
         self.decoded_output_file_path: Path = None
         if type(encoder) == encoders.Vtm:
-            self.decoded_output_file_path: Path = output_dir_path / f"{base_filename}_decoded.yuv"
+            self.decoded_output_file_path: Path = self.output_dir_path / f"{self.base_filename}_decoded.yuv"
 
     @property
     def needs_encoding(self):
@@ -78,6 +73,9 @@ class EncodingRun:
             return not self.output_file.get_filepath().exists() or "encoding_time" not in self.metrics
         elif cfg.Cfg().overwrite_encoding == cfg.ReEncoding.OFF:
             return not self.output_file.get_filepath().exists() and not self.metrics.has_calculated_metrics
+
+    def get_log_path(self, type_: str):
+        return self.output_dir_path / f"{self.base_filename}_{type_}_log.txt"
 
     def __eq__(self,
                other: EncodingRun):

tester/core/tester.py
@@ -252,11 +252,15 @@ class Tester:
                         )
 
                         metric = encoding_run.metrics
-                        psnr_needed = "psnr" not in metric and global_psnr
-                        ssim_needed = "ssim" not in metric and global_ssim
-                        vmaf_needed = "vmaf" not in metric and global_vmaf
+                        needed_metrics = []
+                        if "psnr" not in metric and global_psnr:
+                            needed_metrics.append("psnr")
+                        if "ssim" not in metric and global_ssim:
+                            needed_metrics.append("ssim")
+                        if "vmaf" not in metric and global_vmaf:
+                            needed_metrics.append("vmaf")
                         conformance_needed = "conforms" not in metric and global_conformance
-                        arguments = (encoding_run, metric, psnr_needed, ssim_needed, vmaf_needed, conformance_needed,
+                        arguments = (encoding_run, metric, needed_metrics, conformance_needed,
                                      Cfg().remove_encodings_after_metric_calculation)
                         if parallel_calculations > 1:
                             values.append(arguments)
@@ -272,7 +276,7 @@ class Tester:
 
     @staticmethod
     def _calculate_metrics_for_one_run(in_args):
-        encoding_run, metrics, psnr_needed, ssim_needed, vmaf_needed, conf, remove_encoding = in_args
+        encoding_run, metrics, needed_metrics, conf, remove_encoding = in_args
         try:
             console_log.info(f"Tester: Computing metrics for '{encoding_run.name}'")
 
@@ -289,19 +293,13 @@ class Tester:
                     # TODO: implement for other codecs
                     metrics["conforms"] = False
 
-            if psnr_needed or ssim_needed or vmaf_needed:
-                psnr_avg, ssim_avg, vmaf_avg = ffmpeg.compute_metrics(
+            if needed_metrics:
+                res = ffmpeg.compute_metrics(
                     encoding_run,
-                    psnr=psnr_needed,
-                    ssim=ssim_needed,
-                    vmaf=vmaf_needed
+                    needed_metrics
                 )
-                if psnr_needed:
-                    metrics["psnr"] = psnr_avg
-                if ssim_needed:
-                    metrics["ssim"] = ssim_avg
-                if vmaf_needed:
-                    metrics["vmaf"] = vmaf_avg
+                for m in needed_metrics:
+                    metrics[m] = res[m]
             else:
                 console_log.info(f"Tester: Metrics for '{encoding_run.name}' already exist")
 

tester/encoders/base.py
@@ -318,7 +318,7 @@ class EncoderBase:
         console_log.debug(f"{self._name}: Encoding file '{encoding_run.input_sequence.get_filepath().name}'")
         console_log.debug(f"{self._name}: Output: '{encoding_run.output_file.get_filepath().name}'")
         console_log.debug(f"{self._name}: Arguments: '{encoding_run.param_set.to_cmdline_str()}'")
-        console_log.debug(f"{self._name}: Log: '{encoding_run.encoding_log_path.name}'")
+        console_log.debug(f"{self._name}: Log: '{encoding_run.get_log_path('encoding').name}'")
 
         if not encoding_run.needs_encoding:
             console_log.info(f"{self._name}: File '{encoding_run.output_file.get_filepath().name}' already exists")
@@ -363,7 +363,7 @@ class EncoderBase:
                 stderr=subprocess.STDOUT,
                 stdin=ffmpeg_pipe.stdout if ffmpeg_pipe else None
             )
-            with encoding_run.encoding_log_path.open("w") as encoding_log:
+            with encoding_run.get_log_path('encoding').open("w") as encoding_log:
                 encoding_log.write(output.decode())
         except subprocess.CalledProcessError as exception:
             console_log.error(f"{self._name}: Encoding failed "

commit 320b3aabeb43a0006fe6f26c01417b28fb6755fc

Fix bug in vmaf result collection introduced in 773da45c2ecb21395878d8b33909f3d80ca4d55c

tester/core/ffmpeg.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import json
 import os
 import re
 import shutil
@@ -44,7 +45,7 @@ def ffmpeg_validate_config():
 
 def compute_metrics(encoding_run: test.EncodingRun, metrics: list) -> Dict[str: float]:
     if not metrics:
-        return None, None, None
+        return {}
 
     assert encoding_run.output_file.get_filepath().exists()
     # Absolute paths were causing trouble, so use relative paths.
@@ -169,6 +170,10 @@ def compute_metrics(encoding_run: test.EncodingRun, metrics: list) -> Dict[str: 
 
         results = {}
         for metric in metrics:
+            if metric == "vmaf":
+                results[metric] = json.load(logs[metric].open("r"))["VMAF score"]
+                continue
+
             frame_results = []
             with logs[metric].open("r") as log:
                 lines = log.readlines()